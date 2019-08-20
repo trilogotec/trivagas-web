ddc0da1ea5d0e1ca5db149a93abff0f9ecbac94d
Hugo Ramos <hugo@trilogo.com.br>
2019-08-20 19:00:43 -0300

    .

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                header...
+            </div>
+        )
+    }
+}
+
+export default Header;
