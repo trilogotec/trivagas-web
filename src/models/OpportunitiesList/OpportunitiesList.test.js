85cc89728e5d3cf5edb345f7d6f92a5a9b90f6d9
Hiléo Andersson <hileomsi@gmail.com>
2019-11-28 13:50:10 -0300

    fix:test model

--- src/models/OpportunitiesList/OpportunitiesList.test.js
+++ src/models/OpportunitiesList/OpportunitiesList.test.js
@@ -1,7 +1,6 @@
 import Immutable from 'seamless-immutable';
 import OpportunitiesListModel from './OpportunitiesList.model';
 
-
 const defaultState = Immutable({
   count: 0,
 });
@@ -11,9 +10,6 @@ describe("OpportunitiesListModel model", () => {
     const payload = 10;
     const expectState = { count: 10 };
 
-    store.dispatch.opportunitiesList.increment(payload);
-
-    expect(store.getState().opportunitiesList).toBe(expectState);
     expect(OpportunitiesListModel.reducers.increment(defaultState, payload)).toEqual(expectState);
   });
 });
