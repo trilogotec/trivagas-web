bea34600ee9aca3243402be0b47045df3e4d422e
Hiléo Andersson <hileomsi@gmail.com>
2019-11-30 10:21:58 -0300

    ajeitando testes

--- src/components/OpportunityCard/OpportunityCard.test.js
+++ src/components/OpportunityCard/OpportunityCard.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import OpportunityCard from './OpportunityCard';
 
+const defaultProps = {
+  company: {
+    id: 1,
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/sindresorhus/128.jpg',
+    name: 'Trilogo',
+    region: 'Fortaleza, Ceará, Brasil',
+  },
+  title: 'UI/UX Designer',
+  description: 'DailyRounds is Indias largest Doctor network. (Think StackOverflow for Doctors) Over 500,000+ Doctors, nurses and other healthcare practitioners DailyRounds is Indias largest Doctor network. (Think StackOverflow for Doctors) Over 500,000+ Doctors, nurses and other healthcare practitioners',
+  workType: 'Full time',
+  createdAt: 'Há um dia',
+};
+
 describe('Testing OpportunityCard Component', () => {
   it('renders without crashing', () => {
-    shallow(<OpportunityCard />);
+    shallow(<OpportunityCard  {...defaultProps} />);
   });
 });
--- src/containers/Opportunities/__tests__/OpportunitiesList.test.js
+++ src/containers/Opportunities/__tests__/OpportunitiesList.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Immutable from 'seamless-immutable';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
 import OpportunitiesList from '../OpportunitiesList';
 
-describe('Testing Header Component', () => {
-  it('renders without crashing', () => {
-    shallow(<OpportunitiesList />);
+const defaultState = Immutable({
+  opportunitiesList: {
+    loading: false,
+    opportunities: [],
+  },
+});
+
+const storeFake = state => ({
+  default: jest.fn(),
+  subscribe: jest.fn(),
+  dispatch: {
+    opportunitiesList: {
+      findOpportunities: jest.fn(),
+    }
+  },
+  getState: () => state,
+});
+
+
+describe('Testing OpportunitiesList Component', () => {
+  let wrapper;
+  let container;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    const store = storeFake(defaultState);
+
+    wrapper = mount(
+      <BrowserRouter>
+        <Provider store={store}>
+          <OpportunitiesList />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    container = wrapper.find(OpportunitiesList);
+  });
+
+
+  it('should render both the container and the component ', () => {
+    expect(container.length).toBeTruthy();
   });
 });
